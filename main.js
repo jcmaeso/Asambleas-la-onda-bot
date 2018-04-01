da41d599c641c458b07e5e099c804d92f65cff90
Jorge Calatayud Maeso <jorge.cmaeso@alumnos.upm.es>
2018-04-01 23:04:36 +0200

    Vista de teclado

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ const token = '559647678:AAFTXQnd6-VV50Itor04qxHob2FwmAh7FzM';
 const botOptions = {
     polling:{
         params:{
-            timeout: 1000
+            timeout: 100000
         },
     
     }
@@ -39,17 +39,23 @@ bot.onText(/\/start/, (msg,match) => {
 bot.onText(/\/AnadeFecha/, (msg) => {
     let chat_id = msg.chat.id;
     let keyboard = get_day_keyboard(23,32);
-    console.log(keyboard);
     bot.sendMessage(chat_id,"Por favor selecciona la Hora",{
         reply_markup: {
             inline_keyboard: keyboard,
         }
     });
+    console.log("Display Inline Keyboard");
+})
+
+bot.on("error", error =>{
+    console.log(error.msg);
 })
 
 
 let get_day_keyboard = (min,max) =>{
-    let keyboard = get_header("MONTH");
+    let d = new Date();
+    let keyboard = get_header(get_month_name(d.getMonth()));
+    keyboard = keyboard.concat(get_days(d));
     return keyboard;
 }
 let get_header = (header) => {
@@ -62,10 +68,51 @@ let set_btn = (btntext,data) => {
     }
     return {text: btntext,callback_data: data};
 };
-let get_hour_keyboard_item = (hour) =>{
-    return {text: `${hour.toString()}:00`,
-            callback_data: hour.toString(),
-            callback_}
+
+let set_space_btn = () => {
+    return set_btn("FA",null);
+}
+
+let get_days = (date) =>{
+    let weeks = [];
+    let week = padding_week_in(new Date(`${date.getFullYear()}/${date.getMonth()+1}/1`));
+    let c_date;
+    for(let i = 1; i <= get_end_of_month(date.getMonth()+1,date.getFullYear()); i++){
+        c_date = new Date(`${date.getFullYear()}/${date.getMonth()+1}/${i}`);
+        week.push(set_btn(i,i));
+        if(c_date.getDay() === 0){
+            weeks.push(week);
+            week = [];
+        }
+    }
+    week = week.concat(padding_week_out(c_date));
+    weeks.push(week);
+    return weeks;
 };
+
+let padding_week_in = date =>{
+    numOfdays = date.getDay() === 0 ? 6 : date.getDay()-1;
+    let padding = new Array(numOfdays);
+    padding.fill(set_space_btn());
+    return padding;
+};
+
+let padding_week_out = date =>{
+    numOfdays = date.getDay() === 0 ? 0 : 7-date.getDay();
+    let padding = new Array(numOfdays);
+    padding.fill(set_space_btn());
+    return padding;
+};
+
+let get_end_of_month = (month,year) =>{
+    return new Date(year, month, 0).getDate();
+}
+
+let get_month_name = month => {
+    const monthNames = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio",
+  "Julio", "Agosto", "Septiembre", "Octubre", "Noviembew", "Diciembre"];
+    return monthNames[month];
+
+}
 
 
